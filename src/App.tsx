b11831f6c0e4299b6518e4418c0672ef111d2565
Farhan Alam <metalhead901@live.com>
2021-06-06 00:46:29 +0600

    create devices component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@
 import * as React from 'react';
 import { Fragment } from 'react';
 import Login from './components/Login';
+import Devices from './components/Devices';
 
 interface Props {
 
@@ -16,12 +17,21 @@ class App extends React.Component<Props> {
 		isLogin: false,
 	};
 
-	
-
 	render() {
+		const { isLogin } = this.state;
+
+		const display = isLogin ?
+			(
+				<Devices />
+			)
+			:
+			(
+				<Login onLogin={(e) => { this.setState({ isLogin: e }) }} />
+			)
+
 		return (
 			<div className='container'>
-				<Login onLogin={(e) => { this.setState({ isLogin: e }) }} />
+				{display}
 			</div>
 		);
 	}
---
+++ src/components/Devices/index.tsx
@@ -0,0 +1,31 @@
+import * as React from 'react';
+import { Fragment } from 'react';
+
+interface Props {
+
+}
+
+interface State {
+    user: string,
+    pass: string,
+    isErr: boolean
+}
+
+export default class Devices extends React.Component<Props> {
+    state: State = {
+        user: '',
+        pass: '',
+        isErr: false,
+    };
+
+    render() {
+        const { user, pass, isErr } = this.state;
+
+        return (
+            <div className="">
+                test
+            </div>
+        )
+    }
+
+}
