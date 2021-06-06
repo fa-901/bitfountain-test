8290bcb0a76069bc80c8748ecfdf235f03df7190
Farhan Alam <metalhead901@live.com>
2021-06-06 15:38:12 +0600

    Updates

--- src.f69400ca.js
+++ src.f69400ca.js
@@ -71485,7 +71485,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61296" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55423" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
